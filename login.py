11aea3c8f2e7e03b09cbd95925d6356f787809d8
do thai son <dothaison1@gmail.com>
2025-06-15 11:33:28 +0700

    add login

---
+++ login.py
@@ -0,0 +1,7 @@
+userName = input("Enter username: ")
+passWord = input("Enter your password: ")
+
+if userName and passWord:
+    print("Login successful")
+else:
+    print("Username or password cannot be empty")
